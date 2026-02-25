a26537325d2e85efa3c0660cb13bcc106d67df79
traliach <ali.achille.traore@gmail.com>
2026-02-24 21:25:13 -0500

    Add localStorage persistence

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
 import listReducer from './listSlice'
 
+function loadList() {
+  try {
+    const raw = localStorage.getItem('mangaList')
+    return raw ? { list: JSON.parse(raw) } : undefined
+  } catch {
+    return undefined
+  }
+}
+
 export const store = configureStore({
-  reducer: {
-    list: listReducer,
-  },
+  reducer: { list: listReducer },
+  preloadedState: loadList(),
+})
+
+store.subscribe(() => {
+  localStorage.setItem('mangaList', JSON.stringify(store.getState().list))
 })
 
 export type RootState = ReturnType<typeof store.getState>
